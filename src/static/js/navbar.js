9d61db61ffe8b87b334c34b3c5af344ad92268f4
vatsalbansal123 <bansalvatsal@gmail.com>
2021-03-18 17:03:38 +0530

    made some changes

---
+++ src/static/js/navbar.js
@@ -0,0 +1,10 @@
+navbar = document.getElementsByClassName('navbar-header')[0];
+
+window.addEventListener('DOMContentLoaded', navbarAnimation = () => {
+    navbar.classList.add('navbar-animation');
+});
+
+// if(navbar.classList.contains('navbar-animation')){
+//     /* animation: amination-name animation-duration animation-iteration-count; */
+//     navbar.style.animation: '2.5s 1 '
+// }
