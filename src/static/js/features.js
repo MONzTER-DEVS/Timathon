0b401fd9b4821911d69f715d69e68a68d095fd38
vatsalbansal123 <bansalvatsal@gmail.com>
2021-03-19 20:47:13 +0530

    complete with the home page

---
+++ src/static/js/features.js
@@ -0,0 +1,15 @@
+const featureScroll = 600
+const headingText = document.getElementsByClassName('heading-one-feature')[0];
+const underline = document.getElementsByClassName('underline-blue')[0];
+const paraText = document.getElementsByClassName('para-one-feature')[0];
+const image = document.getElementsByClassName('image-feature-one')[0];
+const elements = [headingText, underline, paraText, image]
+
+if (document.documentElement.scrollTop > featureScroll){
+    headingText.classList.add('feature-animation');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.add('animation-fade-up');
+    }
+}
+
+console.log('DEBUG');
