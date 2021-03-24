4b6f862d431c22c4f8b9a941609a428a8e90ed35
lakshyagarg911 <lakshyagarg911@gmail.com>
2021-03-24 11:57:44 +0530

    added Excel support

--- src/home/views.py
+++ src/home/views.py
@@ -1,30 +1,51 @@
+import pandas as pd
+import os
+import openpyxl
+import pprint
 from django.shortcuts import render, redirect, reverse
 from .forms import TableDataForm
 from django.http import HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
-import pandas as pd
-import os
 from django.urls import reverse_lazy
 from timathon.settings import MEDIA_ROOT
+
 # Create your views here.
 table_data = []
 
+
 # the home page
 def index(request):
-    return render(request, '../templates/home/index.html')
+    return render(request, 'home/index.html')
+
 
 @login_required
 def components(request):
-    return render(request, '../templates/components/components.html')
+    return render(request, 'components/components.html')
 
 
 def favourites(request):
-    return render(request, '../templates/home/favourites.html')
+    return render(request, 'home/favourites.html')
 
 
 def charts(request):
-    return render(request, '../templates/components/charts/charts.html')
+    return render(request, 'components/charts/charts.html')
+
+
+def read_excel(fp):
+    wb = openpyxl.load_workbook(fp)
+    sheet = wb.active
+    num_of_columns = sheet.max_column
+    num_of_rows = sheet.max_row
+
+    all_rows = []
+    for row in range(1, num_of_rows + 1):
+        this_row = []
+        for col in range(1, num_of_columns + 1):
+            this_row.append(sheet.cell(row=row, column=col).value)
+
+        all_rows.append(this_row)
+    return all_rows
 
 
 # def tables(request):
@@ -51,16 +72,26 @@ def tables(request):
             fs = FileSystemStorage()
             fs.save(uploaded_file.name, uploaded_file)
             form.save()
-            data = pd.read_csv(os.path.join(MEDIA_ROOT, uploaded_file.name))
+            fn = os.path.join(MEDIA_ROOT, uploaded_file.name)
+            if fn.endswith('csv'):
+                data = pd.read_csv(os.path.join(MEDIA_ROOT, uploaded_file.name))
+
+            elif fn.endswith('xls') or fn.endswith('xlsx'):
+                raw = read_excel(fn)
+                data = pd.DataFrame(raw)
+
             table_data.append(data)
             return HttpResponseRedirect(reverse_lazy('home:table_results'))
     else:
         data = None
         form = TableDataForm()
     context = {"form": form}
-    return render(request, '../templates/components/tables/tables.html', context)
+    return render(request, 'components/tables/tables.html', context)
 
 
 def table_result(request):
-    context = {'data': table_data[-1]}
-    return render(request, '../templates/components/table_results/results.html', context)
+    try:
+        context = {'data': table_data[-1]}
+    except Exception:
+        context = {'data': []}
+    return render(request, 'components/table_results/results.html', context)
