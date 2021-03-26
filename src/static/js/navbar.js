b6735d2d5c093b8174e29d558b51a56a6ac31ab1
iMADi-ARCH <nandanaditya985@gmail.com>
2021-03-26 13:45:46 +0530

    Fixed the navbar

--- src/static/js/navbar.js
+++ src/static/js/navbar.js
@@ -1,16 +1,19 @@
 // console.log("Testing!");
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".navbar-div");
 
-const colorNav = '#e9eadb';
+const colorNav = 'rgba(0, 0, 0, 0.5)';
 const tempNavHeight = 120;
 function changeColorNav(){
     if (nav.getBoundingClientRect().top + document.documentElement.scrollTop > tempNavHeight) {
         nav.style.backgroundColor = colorNav;
+        // nav.style.backDrop = "blur(10px)";
+        nav.style.boxShadow = "0 0 1em #000000"
         nav.style.transition = "all 0.4s ease-in";
         nav.style.zIndex = "5"
     } else {
         nav.style.transition = "all 0.4s ease-in";
         nav.style.backgroundColor = "transparent";
+        nav.style.boxShadow = ""
         nav.style.border = 0;
     }
 }
